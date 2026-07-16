2312605f36277c3c7c951204aef446d7503d6b85
sanbisensei <lsunbi1@gmail.com>
2026-07-17 00:21:08 +0600

    single cake page bug fixed and review is now working smoothly

--- src/app/cakes/[slug]/page.tsx
+++ src/app/cakes/[slug]/page.tsx
@@ -5,6 +5,7 @@ import ReviewSection from "@/components/ui/ReviewSection";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
+//using nextjs 15
 type Props = {
   params: Promise<{ slug: string }>;
 };
